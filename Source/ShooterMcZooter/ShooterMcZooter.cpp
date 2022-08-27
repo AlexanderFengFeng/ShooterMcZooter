4183f1523be9bc8b196f0c3f35d8724a47e1821b
AlexanderFengFeng <39276953+AlexanderFengFeng@users.noreply.github.com>
2022-08-27 00:31:21 -0700

    Initial commit

---
+++ Source/ShooterMcZooter/ShooterMcZooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShooterMcZooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShooterMcZooter, "ShooterMcZooter" );
---
+++ Source/ShooterMcZooter/ShooterMcZooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterMcZooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERMCZOOTER_API AShooterMcZooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
